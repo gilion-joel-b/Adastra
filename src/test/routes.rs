5773e5bb65a9d8a020fec5652d8d02f329075979
Joel W. Blomster <joel.blomster@gilion.com>
2024-06-19 13:46:47 +0200

    formatting

--- src/test/routes.rs
+++ src/test/routes.rs
@@ -1,8 +1,10 @@
 use axum::{routing::get, Router};
 
 pub fn router() -> Router {
-    Router::new().route("/", get(|| async { "Hello, World!" }))
-        .route("/hello/:name", get(|params: axum::extract::Path<(String,)>| async {
-            params.0
-        }))
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route(
+            "/hello/:name",
+            get(|params: axum::extract::Path<(String,)>| async { params.0 }),
+        )
 }
