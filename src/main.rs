1bc23782c784af288728f8d1a383b1a40871d27d
Joel W. Blomster <joel.blomster@gilion.com>
2024-07-04 18:24:01 +0200

    reorganize server

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
+use std::net::{Ipv4Addr, SocketAddrV4};
+
+use axum::Router;
+
 mod server;
 mod test;
 mod screen;
 
-use server::AppServer;
+const LOCALHOST: SocketAddrV4 = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8080);
 
 #[tokio::main]
-async fn main() {
-    let app = AppServer::new().await;
-    setup_routes(app).serve().await;
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let listener = tokio::net::TcpListener::bind(LOCALHOST).await?;
+    let app: Router = Router::new();
+
+
+    Ok(axum::serve::serve(listener, app).await?)
 }
 
-fn setup_routes(app: AppServer) -> AppServer {
-    app.merge(test::routes::router())
-        .merge(screen::routes::router())
-}
